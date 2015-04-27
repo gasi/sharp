1a1248eb6a08a4fc030dc8d002410059fa526952
Daniel Gasienica <daniel@fiftythree.com>
2015-04-27 13:11:03 -0700

    Improve style

--- test/unit/composite.c
+++ test/unit/composite.c
@@ -13,15 +13,15 @@ main( int argc, char **argv )
   GOptionGroup *main_group;
   GError *error = NULL;
 
-  VipsImage *image1Input;
-  VipsImage *image1Colors;
-  VipsImage *image1Alpha;
-  VipsImage *image1AlphaNormalized;
+  VipsImage *srcInput;
+  VipsImage *srcRGB;
+  VipsImage *srcAlpha;
+  VipsImage *srcAlphaNormalized;
 
-  VipsImage *image2Input;
-  VipsImage *image2Colors;
-  VipsImage *image2Alpha;
-  VipsImage *image2AlphaNormalized;
+  VipsImage *dstInput;
+  VipsImage *dstRGB;
+  VipsImage *dstAlpha;
+  VipsImage *dstAlphaNormalized;
 
   VipsImage *out;
   VipsImage *outRGB;
@@ -31,10 +31,16 @@ main( int argc, char **argv )
   const int ALPHA_BAND_INDEX = 3;
   const int NUM_COLOR_BANDS = 3;
 
-  if( VIPS_INIT( argv[0] ) )
+  // Main
+  char* programName = argv[0];
+  char* srcPath = argv[1];
+  char* dstPath = argv[2];
+  char* outPath = argv[3];
+
+  if( VIPS_INIT( programName ) )
     vips_error_exit( NULL );
 
-  context = g_option_context_new( "composite in1 in2 out" );
+  context = g_option_context_new( "src dst out" );
 
   main_group = g_option_group_new( NULL, NULL, NULL, NULL, NULL );
   g_option_context_set_main_group( context, main_group );
@@ -51,49 +57,48 @@ main( int argc, char **argv )
   }
 
   if( argc != 4 )
-    vips_error_exit( "usage: %s in1 in2 out", argv[0] );
+    vips_error_exit( "usage: %s src dst out", programName );
 
-  if( !(image1Input = vips_image_new_from_file( argv[1], NULL )) )
+  if( !(srcInput = vips_image_new_from_file( srcPath, NULL )) )
     vips_error_exit( NULL );
 
-  if( !(image2Input = vips_image_new_from_file( argv[2], NULL )) )
+  if( !(dstInput = vips_image_new_from_file( dstPath, NULL )) )
     vips_error_exit( NULL );
 
   // Extract RGB bands for image 1
-  if( vips_extract_band( image1Input, &image1Colors, 0, "n",
-        NUM_COLOR_BANDS, NULL ) )
+  if( vips_extract_band( srcInput, &srcRGB, 0, "n", NUM_COLOR_BANDS, NULL ) )
     vips_error_exit( NULL );
 
   // Extract RGB bands for image 2
-  if( vips_extract_band( image2Input, &image2Colors, 0, "n",
-        NUM_COLOR_BANDS, NULL ) )
+  if( vips_extract_band( dstInput, &dstRGB, 0, "n", NUM_COLOR_BANDS, NULL ) )
     vips_error_exit( NULL );
 
   // Extract alpha band for image 1
-  if( vips_extract_band( image1Input, &image1Alpha, ALPHA_BAND_INDEX, NULL ) )
+  if( vips_extract_band( srcInput, &srcAlpha, ALPHA_BAND_INDEX, NULL ) )
     vips_error_exit( NULL );
 
   // Extract alpha band for image 2
-  if( vips_extract_band( image2Input, &image2Alpha, ALPHA_BAND_INDEX, NULL ) )
+  if( vips_extract_band( dstInput, &dstAlpha, ALPHA_BAND_INDEX, NULL ) )
     vips_error_exit( NULL );
 
-  printf("Image 1 dimensions: %dx%d\n", vips_image_get_width(image1Input),
-    vips_image_get_height(image1Input));
-  printf("Image 2 dimensions: %dx%d\n", vips_image_get_width(image2Input),
-    vips_image_get_height(image2Input));
-  printf("image1Alpha->Bands: %d\n", image1Alpha->Bands);
-  printf("image2Alpha->Bands: %d\n", image2Alpha->Bands);
-  printf("image1Colors->Bands: %d\n", image1Colors->Bands);
-  printf("image2Colors->Bands: %d\n", image2Colors->Bands);
+  printf("src dimensions: %dx%d\n", vips_image_get_width(srcInput),
+    vips_image_get_height(srcInput));
+  printf("dst dimensions: %dx%d\n", vips_image_get_width(dstInput),
+    vips_image_get_height(dstInput));
+  printf("srcAlpha->Bands: %d\n", srcAlpha->Bands);
+  printf("dstAlpha->Bands: %d\n", dstAlpha->Bands);
+  printf("srcRGB->Bands: %d\n", srcRGB->Bands);
+  printf("dstRGB->Bands: %d\n", dstRGB->Bands);
 
-  if( vips_linear1( image1Alpha, &image1AlphaNormalized,
-        1.0 / 255.0, 0.0, NULL ) )
+  if( vips_linear1( srcAlpha, &srcAlphaNormalized, 1.0 / 255.0, 0.0, NULL ) )
     vips_error_exit( NULL );
 
-  if( vips_linear1( image2Alpha, &image2AlphaNormalized,
-        1.0 / 255.0, 0.0, NULL ) )
+  if( vips_linear1( dstAlpha, &dstAlphaNormalized, 1.0 / 255.0, 0.0, NULL ) )
     vips_error_exit( NULL );
 
+  //
+  // Compute normalized output alpha channel:
+  //
   // References:
   // - http://en.wikipedia.org/wiki/Alpha_compositing#Alpha_blending
   // - https://github.com/jcupitt/ruby-vips/issues/28#issuecomment-9014826
@@ -105,11 +110,14 @@ main( int argc, char **argv )
   //                         t2
   VipsImage *t1;
   VipsImage *t2;
-  if( vips_linear1( image1AlphaNormalized, &t1, -1.0, 1.0, NULL ) ||
-      vips_multiply( image2AlphaNormalized, t1, &t2, NULL ) ||
-      vips_add( image1AlphaNormalized, t2, &outAlphaNormalized, NULL ) )
+  if( vips_linear1( srcAlphaNormalized, &t1, -1.0, 1.0, NULL ) ||
+      vips_multiply( dstAlphaNormalized, t1, &t2, NULL ) ||
+      vips_add( srcAlphaNormalized, t2, &outAlphaNormalized, NULL ) )
     vips_error_exit( NULL );
 
+  //
+  // Compute output RGB channels:
+  //
   // out_rgb = (src_rgb * src_a + dst_rgb * dst_a * (1 - src_a)) / out_a
   //            ^^^^^^^^^^^^^^^
   //                  t3
@@ -120,8 +128,8 @@ main( int argc, char **argv )
   VipsImage *t3;
   VipsImage *t4;
   VipsImage *t5;
-  if ( vips_multiply( image1Colors, image1AlphaNormalized, &t3, NULL ) ||
-       vips_multiply( image2Colors, t2, &t4, NULL ) ||
+  if ( vips_multiply( srcRGB, srcAlphaNormalized, &t3, NULL ) ||
+       vips_multiply( dstRGB, t2, &t4, NULL ) ||
        vips_add( t3, t4, &t5, NULL ) ||
        vips_divide( t5, outAlphaNormalized, &outRGB, NULL) )
     vips_error_exit( NULL );
@@ -132,26 +140,27 @@ main( int argc, char **argv )
   if( vips_bandjoin2( outRGB, outAlpha, &out, NULL ) )
     vips_error_exit( NULL );
 
-  // Temporary variables
+  // Intermediate results:
   g_object_unref( t1 );
   g_object_unref( t2 );
   g_object_unref( t3 );
   g_object_unref( t4 );
   g_object_unref( t5 );
 
-  // Image 1
-  g_object_unref( image1Input );
-  g_object_unref( image1Colors );
-  g_object_unref( image1Alpha );
-  g_object_unref( image1AlphaNormalized );
+  // Source
+  g_object_unref( srcInput );
+  g_object_unref( srcRGB );
+  g_object_unref( srcAlpha );
+  g_object_unref( srcAlphaNormalized );
 
-  // Image 2
-  g_object_unref( image2Input );
-  g_object_unref( image2Colors );
-  g_object_unref( image2Alpha );
-  g_object_unref( image2AlphaNormalized );
+  // Destination
+  g_object_unref( dstInput );
+  g_object_unref( dstRGB );
+  g_object_unref( dstAlpha );
+  g_object_unref( dstAlphaNormalized );
 
-  if( vips_image_write_to_file( out, argv[3], NULL ) )
+  // Output
+  if( vips_image_write_to_file( out, outPath, NULL ) )
     vips_error_exit( NULL );
 
   // Output
