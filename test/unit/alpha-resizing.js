70d41f14a351b320462f855294ae915d543acdcf
Daniel Gasienica <daniel@fiftythree.com>
2015-05-05 16:02:11 -0700

    Add black fringing repro
    When resizing an image with alpha channel, it creates black fringes around
    sharp white edges.

---
+++ test/unit/alpha-resizing.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var sharp = require('../../index');
+var fixtures = require('../fixtures');
+
+sharp.cache(0);
+
+describe('Resizing image with alpha channel', function() {
+
+  it('should not output black fringing around white details [Photoshop]', function(done) {
+    var width = 512;
+    var height = 512;
+    var interpolator = 'nearest';
+    var outFilename =  fixtures.path('alpha-resizing-photoshop-out-' +
+      width + 'x' + height + '-' + interpolator + '.png');
+
+    sharp(fixtures.path('alpha-resizing-photoshop-1024x1024.png'))
+      .resize(width, height)
+      .interpolateWith(sharp.interpolator[interpolator])
+      .toFile(outFilename, done);
+  });
+
+  it('should not output black fringing around white details [Paper]', function(done) {
+    var width = 1024;
+    var height = 768;
+    var interpolator = 'nearest';
+    var outFilename = fixtures.path('alpha-resizing-paper-out-' +
+      width + 'x' + height + '-' + interpolator + '.png');
+
+    sharp(fixtures.path('alpha-resizing-paper-2048x1536.png'))
+      .resize(width, height)
+      .interpolateWith(sharp.interpolator[interpolator])
+      .toFile(outFilename, done);
+  });
+
+});
