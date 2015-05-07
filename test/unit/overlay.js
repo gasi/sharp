bec34be54c5ad9cfd1dce6bf35c51497e437d6e4
Daniel Gasienica <daniel@fiftythree.com>
2015-05-06 20:17:58 -0700

    Add test for low-alpha 01 overlay

--- test/unit/overlay.js
+++ test/unit/overlay.js
@@ -25,6 +25,20 @@ describe('Overlays', function() {
       });
   });
 
+  it('Overlay low-alpha transparent PNG on solid background', function(done) {
+    var BASE_NAME = 'alpha-layer-01-low-alpha.png';
+    var actual = fixtures.path('output.' + BASE_NAME);
+    var expected = fixtures.expected(BASE_NAME);
+
+    sharp(fixtures.inputPngOverlayLayer0)
+      .overlayWith(fixtures.inputPngOverlayLayer1LowAlpha)
+      .toFile(expected, function (error) {
+        if (error) return done(error);
+
+        fixtures.assertIdentical(expected, expected, done);
+      });
+  });
+
   it('Composite three transparent PNGs into one', function(done) {
     var BASE_NAME = 'alpha-layer-012.png';
     var actual = fixtures.path('output.' + BASE_NAME);
