23db1cd818bd83b69b9fa9ca14078efb76ea42b3
Daniel Gasienica <daniel@fiftythree.com>
2015-05-06 17:59:52 -0700

    Premultiply alpha channel for transparent images
    This avoids dark edges when transforming and compositing images with
    alpha channel.
    
    References:
    - http://entropymine.com/imageworsener/resizealpha/
    - https://github.com/jcupitt/libvips/issues/291
    
    TODO: Test with composition and ensure there is no double premultiplication.

--- src/resize.cc
+++ src/resize.cc
@@ -452,6 +452,33 @@ class ResizeWorker : public NanAsyncWorker {
       image = greyscale;
     }
 
+    // Premultiply image alpha channel before all transformations to avoid
+    // dark fringing around bright pixels:
+    // See: http://entropymine.com/imageworsener/resizealpha/
+    bool shouldPremultiplyImageAlpha = HasAlpha(image) && image->Bands == 4;
+    if (shouldPremultiplyImageAlpha) {
+        VipsImage *imageRGB;
+        VipsImage *imageAlpha;
+        VipsImage *imageAlphaNormalized;
+        VipsImage *imageRGBPremultiplied;
+        VipsImage *imagePremultiplied;
+        if (vips_extract_band(image, &imageRGB, 0, "n", 3, NULL) ||
+            vips_extract_band(image, &imageAlpha, 3, "n", 1, NULL) ||
+            vips_linear1(imageAlpha, &imageAlphaNormalized, 1.0 / 255.0, 0.0, NULL) ||
+            vips_multiply(imageRGB, imageAlphaNormalized, &imageRGBPremultiplied, NULL) ||
+            vips_bandjoin2(imageRGBPremultiplied, imageAlpha, &imagePremultiplied, NULL)) {
+          (baton->err).append("Failed to premultiply alpha channel.");
+          return Error();
+        }
+        vips_object_local(hook, imageRGB);
+        vips_object_local(hook, imageAlpha);
+        vips_object_local(hook, imageAlphaNormalized);
+        vips_object_local(hook, imageRGBPremultiplied);
+        vips_object_local(hook, imagePremultiplied);
+
+        image = imagePremultiplied;
+    }
+
     if (xshrink > 1 || yshrink > 1) {
       VipsImage *shrunk;
       // Use vips_shrink with the integral reduction
@@ -838,6 +865,30 @@ class ResizeWorker : public NanAsyncWorker {
       }
       vips_object_local(hook, composited);
       image = composited;
+    }
+
+    // Reverse premultiplication after all transformations:
+    if (shouldPremultiplyImageAlpha) {
+        VipsImage *imageAlphaTransformed;
+        VipsImage *imageAlphaNormalizedTransformed;
+        VipsImage *imageRGBPremultipliedTransformed;
+        VipsImage *imageRGBUnpremultipliedTransformed;
+        VipsImage *imageUnpremultiplied;
+        if (vips_extract_band(image, &imageRGBPremultipliedTransformed, 0, "n", 3, NULL) ||
+            vips_extract_band(image, &imageAlphaTransformed, 3, "n", 1, NULL) ||
+            vips_linear1(imageAlphaTransformed, &imageAlphaNormalizedTransformed, 1.0 / 255.0, 0.0, NULL) ||
+            vips_divide(imageRGBPremultipliedTransformed, imageAlphaNormalizedTransformed, &imageRGBUnpremultipliedTransformed, NULL) ||
+            vips_bandjoin2(imageRGBUnpremultipliedTransformed, imageAlphaTransformed, &imageUnpremultiplied, NULL)) {
+          (baton->err).append("Failed to reverse premultiplication of alpha channel.");
+          return Error();
+        }
+        vips_object_local(hook, imageRGBPremultipliedTransformed);
+        vips_object_local(hook, imageAlphaTransformed);
+        vips_object_local(hook, imageAlphaNormalizedTransformed);
+        vips_object_local(hook, imageRGBUnpremultipliedTransformed);
+        vips_object_local(hook, imageUnpremultiplied);
+
+        image = imageUnpremultiplied;
     }
 
     // Convert image to sRGB, if not already
