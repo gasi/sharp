01c164e81820c12dd068e13635c3e17cc54ce3bf
Daniel Gasienica <daniel@fiftythree.com>
2015-05-14 15:35:29 -0700

    Simplify overlay tests

--- test/unit/overlay.js
+++ test/unit/overlay.js
@@ -7,39 +7,51 @@ var sharp = require('../../index');
 sharp.cache(0);
 
 
+// Helpers
+var getPaths = function(baseName, extension) {
+  if (typeof extension === 'undefined') {
+    extension = 'png';
+  }
+
+  var actual = fixtures.path('output.' + baseName + '.' + extension);
+  var expected = fixtures.expected(baseName + '.' + extension);
+  var expectedMagick = fixtures.expected(baseName + '.' + extension);
+
+  return {
+    actual: actual,
+    expected: expected,
+    expectedMagick: expectedMagick
+  }
+};
+
+// Test
 describe('Overlays', function() {
   it('Overlay transparent PNG on solid background', function(done) {
-    var BASE_NAME = 'alpha-layer-01.png';
-    var actual = fixtures.path('output.' + BASE_NAME);
-    var expected = fixtures.expected(BASE_NAME);
+    var paths = getPaths('alpha-layer-01');
 
     sharp(fixtures.inputPngOverlayLayer0)
       .overlayWith(fixtures.inputPngOverlayLayer1)
-      .toFile(actual, function (error) {
+      .toFile(paths.actual, function (error) {
         if (error) return done(error);
 
-        fixtures.assertEqual(actual, expected, done);
+        fixtures.assertEqual(paths.actual, paths.expected, done);
       });
   });
 
   it('Overlay low-alpha transparent PNG on solid background', function(done) {
-    var BASE_NAME = 'alpha-layer-01-low-alpha.png';
-    var actual = fixtures.path('output.' + BASE_NAME);
-    var expected = fixtures.expected(BASE_NAME);
+    var paths = getPaths('alpha-layer-01-low-alpha');
 
     sharp(fixtures.inputPngOverlayLayer0)
       .overlayWith(fixtures.inputPngOverlayLayer1LowAlpha)
-      .toFile(actual, function (error) {
+      .toFile(paths.actual, function (error) {
         if (error) return done(error);
 
-        fixtures.assertEqual(actual, expected, done);
+        fixtures.assertEqual(paths.actual, paths.expected, done);
       });
   });
 
   it('Composite three transparent PNGs into one', function(done) {
-    var BASE_NAME = 'alpha-layer-012.png';
-    var actual = fixtures.path('output.' + BASE_NAME);
-    var expected = fixtures.expected(BASE_NAME);
+    var paths = getPaths('alpha-layer-012');
 
     sharp(fixtures.inputPngOverlayLayer0)
       .overlayWith(fixtures.inputPngOverlayLayer1)
@@ -48,47 +60,40 @@ describe('Overlays', function() {
 
         sharp(data)
           .overlayWith(fixtures.inputPngOverlayLayer2)
-          .toFile(actual, function (error) {
+          .toFile(paths.actual, function (error) {
             if (error) return done(error);
 
-            fixtures.assertEqual(actual, expected, done);
+            fixtures.assertEqual(paths.actual, paths.expected, done);
           });
       });
   });
 
-  // This tests that alpha channel unpremultiplication is correct:
   it('Composite two transparent PNGs into one', function(done) {
-    var BASE_NAME = 'alpha-layer-12.png';
-    var actual = fixtures.path('output.' + BASE_NAME);
-    var expected = fixtures.expected(BASE_NAME);
+    var paths = getPaths('alpha-layer-12');
 
     sharp(fixtures.inputPngOverlayLayer1)
       .overlayWith(fixtures.inputPngOverlayLayer2)
-      .toFile(actual, function (error, data, info) {
+      .toFile(paths.actual, function (error, data, info) {
         if (error) return done(error);
-        fixtures.assertEqual(actual, expected, done);
+
+        fixtures.assertEqual(paths.actual, paths.expected, done);
       });
   });
 
-  // This tests that alpha channel unpremultiplication is correct:
   it('Composite two low-alpha transparent PNGs into one', function(done) {
-    var BASE_NAME = 'alpha-layer-12-low-alpha.png';
-    var actual = fixtures.path('output.' + BASE_NAME);
-    var expected = fixtures.expected(BASE_NAME);
+    var paths = getPaths('alpha-layer-12-low-alpha');
 
     sharp(fixtures.inputPngOverlayLayer1LowAlpha)
       .overlayWith(fixtures.inputPngOverlayLayer2LowAlpha)
-      .toFile(actual, function (error, data, info) {
+      .toFile(paths.actual, function (error, data, info) {
         if (error) return done(error);
-        fixtures.assertEqual(actual, expected, done);
+
+        fixtures.assertEqual(paths.actual, paths.expected, done);
       });
   });
 
-  // This tests that alpha channel unpremultiplication is correct:
   it('Composite three low-alpha transparent PNGs into one', function(done) {
-    var BASE_NAME = 'alpha-layer-012-low-alpha.png';
-    var actual = fixtures.path('output.' + BASE_NAME);
-    var expected = fixtures.expected(BASE_NAME);
+    var paths = getPaths('alpha-layer-012-low-alpha');
 
     sharp(fixtures.inputPngOverlayLayer0)
       .overlayWith(fixtures.inputPngOverlayLayer1LowAlpha)
@@ -97,14 +102,14 @@ describe('Overlays', function() {
 
         sharp(data)
           .overlayWith(fixtures.inputPngOverlayLayer2LowAlpha)
-          .toFile(actual, function (error, data, info) {
+          .toFile(paths.actual, function (error, data, info) {
             if (error) return done(error);
-            fixtures.assertEqual(actual, expected, done);
+
+            fixtures.assertEqual(paths.actual, paths.expected, done);
           });
       });
   });
 
-  // This tests that alpha channel unpremultiplication is correct:
   it('Composite transparent PNG onto JPEG', function(done) {
     sharp(fixtures.inputJpg)
       .overlayWith(fixtures.inputPngOverlayLayer1)
