02c8f6820d13573594a4126f2e0bfeb8190fff61
Daniel Gasienica <daniel@fiftythree.com>
2015-05-14 15:45:11 -0700

    Compare overlays against ImageMagick reference images

--- test/unit/overlay.js
+++ test/unit/overlay.js
@@ -7,6 +7,9 @@ var sharp = require('../../index');
 sharp.cache(0);
 
 
+// Constants
+var MAX_ALLOWED_IMAGE_MAGICK_MEAN_SQUARED_ERROR = 0.3;
+
 // Helpers
 var getPaths = function(baseName, extension) {
   if (typeof extension === 'undefined') {
@@ -15,13 +18,35 @@ var getPaths = function(baseName, extension) {
 
   var actual = fixtures.path('output.' + baseName + '.' + extension);
   var expected = fixtures.expected(baseName + '.' + extension);
-  var expectedMagick = fixtures.expected(baseName + '.' + extension);
+  var expectedMagick = fixtures.expected(baseName + '-imagemagick.' + extension);
 
   return {
     actual: actual,
     expected: expected,
     expectedMagick: expectedMagick
   }
+};
+
+var assertEqual = function (paths, callback) {
+  if (typeof callback !== 'function') {
+    throw new TypeError('`callback` must be a function');
+  }
+
+  fixtures.assertEqual(paths.actual, paths.expected, function (error) {
+    if (error) return callback(error);
+
+    sharp.compare(paths.actual, paths.expectedMagick, function (error, info) {
+      if (error) return callback(error);
+
+      if (info.meanSquaredError > MAX_ALLOWED_IMAGE_MAGICK_MEAN_SQUARED_ERROR) {
+        return callback(new Error('Expected MSE against ImageMagick to be <= ' +
+          MAX_ALLOWED_IMAGE_MAGICK_MEAN_SQUARED_ERROR + '. Actual: ' +
+          info.meanSquaredError))
+      }
+
+      callback();
+    })
+  });
 };
 
 // Test
@@ -34,7 +59,7 @@ describe('Overlays', function() {
       .toFile(paths.actual, function (error) {
         if (error) return done(error);
 
-        fixtures.assertEqual(paths.actual, paths.expected, done);
+        assertEqual(paths, done);
       });
   });
 
@@ -46,7 +71,7 @@ describe('Overlays', function() {
       .toFile(paths.actual, function (error) {
         if (error) return done(error);
 
-        fixtures.assertEqual(paths.actual, paths.expected, done);
+        assertEqual(paths, done);
       });
   });
 
@@ -63,7 +88,7 @@ describe('Overlays', function() {
           .toFile(paths.actual, function (error) {
             if (error) return done(error);
 
-            fixtures.assertEqual(paths.actual, paths.expected, done);
+            assertEqual(paths, done);
           });
       });
   });
@@ -76,7 +101,7 @@ describe('Overlays', function() {
       .toFile(paths.actual, function (error, data, info) {
         if (error) return done(error);
 
-        fixtures.assertEqual(paths.actual, paths.expected, done);
+        assertEqual(paths, done);
       });
   });
 
@@ -88,7 +113,7 @@ describe('Overlays', function() {
       .toFile(paths.actual, function (error, data, info) {
         if (error) return done(error);
 
-        fixtures.assertEqual(paths.actual, paths.expected, done);
+        assertEqual(paths, done);
       });
   });
 
@@ -105,7 +130,7 @@ describe('Overlays', function() {
           .toFile(paths.actual, function (error, data, info) {
             if (error) return done(error);
 
-            fixtures.assertEqual(paths.actual, paths.expected, done);
+            assertEqual(paths, done);
           });
       });
   });
